c1acf1c6b58609301d98e95f64abfb095933f6ee
Do0oMores <ws2004421@outlook.com>
2024-05-07 00:57:22 +0800

    构建物品

--- src/main/java/top/desolate/Message.java
+++ src/main/java/top/desolate/Message.java
@@ -1,5 +1,11 @@
 package top.desolate;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -10,13 +16,49 @@ public class Message {
     KillMessage killMessage=new KillMessage();
     FileConfiguration config= killMessage.getConfig();
 
+    NMS nmsUtil;
+
     /**
      *
-     * @param player 玩家
+     * @param item 手上的物品
+     * @param player 获取信息的玩家
+     *
      */
     public void SendMessages(ItemStack item, Player player){
-        ItemMeta meta=item.getItemMeta();
-        String itemName= meta.getDisplayName();
+        //判断空手
+        if (item.getType()!= Material.AIR){
+            ItemMeta meta=item.getItemMeta();
+            if (meta!=null){
+                String itemNBT= nmsUtil.getItemNBT(item);
+                ComponentBuilder builder=new ComponentBuilder();
+                TextComponent itemInfo=new TextComponent("[");
+                //是否有显示名称
+                if (meta.hasDisplayName()){
+                    for (BaseComponent component:TextComponent.fromLegacyText(meta.getDisplayName())){
+                        itemInfo.addExtra(component);
+                    }
+                }else {
+                    boolean tag=true;
+                    if (tag){
+                        if (itemNBT.contains("Enchantments:[")){
+                            itemInfo.setColor(ChatColor.AQUA);
+                        }else {
+                            itemInfo.setColor(ChatColor.WHITE);
+                        }
+                    }else {
+                        if (item.getType().isRecord()||itemNBT.contains("ench:[")){
+                            itemInfo.setColor(ChatColor.AQUA);
+                        }else {
+                            itemInfo.setColor(ChatColor.WHITE);
+                        }
+                    }
+                }
+                itemInfo.addExtra("]");
+                itemInfo.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_ITEM,new BaseComponent[]{new TextComponent(itemNBT)}));
+                builder.append(itemInfo);
+                BaseComponent[] message=builder.create();
+            }
+        }
 
 
 
